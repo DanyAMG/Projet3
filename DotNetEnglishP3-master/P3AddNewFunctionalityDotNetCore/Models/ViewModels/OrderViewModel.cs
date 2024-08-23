42be044fafd33a60743092248e3041e813e974d2
Dany MOTA <dany.mota.goncalves@gmail.com>
2024-08-23 17:47:05 +0200

    Modification des attributs de validation dans la classe OrderViewModel.cs Implémentation de RegularExpression (RegEx) afin de limiter les données dans les champs obligatoires.

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/OrderViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/OrderViewModel.cs
@@ -14,18 +14,23 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         public ICollection<CartLine> Lines { get; set; }
 
         [Required(ErrorMessage = "ErrorMissingName")]
+        [RegularExpression(@"^[a-zA-Zà-ÿÀ-Ÿ\s'-]+$", ErrorMessage = "ErrorInvalidName")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "ErrorMissingAddress")]
+        [Required(ErrorMessage = "ErrorMissingAdress")]
+        [RegularExpression(@"^[a-zA-Z0-9à-ÿÀ-Ÿ\s,.'-]+$", ErrorMessage = "ErrorInvalidAddress")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "ErrorMissingCity")]
+        [RegularExpression(@"^[a-zA-Z\s'-]+$", ErrorMessage = "ErrorInvalidCity")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "ErrorMissingZipCode")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "ErrorInvalidZipCode")]
         public string Zip { get; set; }
 
         [Required(ErrorMessage = "ErrorMissingCountry")]
+        [RegularExpression(@"^[a-zA-Zà-ÿÀ-Ÿ\s-]+$", ErrorMessage = "ErrorInvalidCountry")]
         public string Country { get; set; }
 
         [BindNever]
